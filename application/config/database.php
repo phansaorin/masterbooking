8a1d51e91815071891837f50265574849915491a
sreynak <sreynak.chet@gmail.com>
2014-04-24 10:03:42 +0700

    view package

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'codinga2_booking';
-$db['default']['password'] = '123masterbooking098';
-$db['default']['database'] = 'codinga2_booking';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+$db['default']['database'] = 'codingat_booking';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
